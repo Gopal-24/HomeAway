65e6cbfc4974d86b81b7bde58f53e29739064283
Gopal-24 <megopalkhamnekar@gmail.com>
2025-10-22 13:48:55 +0530

    Error Handling

--- app.js
+++ app.js
@@ -50,10 +50,14 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 // create route
-app.post("/listings", async (req, res) => {
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
+app.post("/listings", async (req, res, next) => {
+  try {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  } catch (err) {
+    next(err);
+  }
 });
 
 // edit route
@@ -97,6 +101,10 @@ app.delete("/listings/:id", async (req, res) => {
 //   console.log("Sample was saved");
 //   res.send("Successful");
 // });
+
+app.use((err, req, res, next) => {
+  res.send("Something went wrong");
+});
 
 app.listen(8080, () => {
   console.log("Server is listening on port 8080");
